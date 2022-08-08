6cede88b4bfcb7909310e2e8bc669d7aa24ba869
DESKTOP-SSC4UP3\huilianyun <1090624761@qq.com>
2022-08-08 15:23:29 +0800

    first blood

---
+++ config-server/src/test/java/top/lovelily/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package top.lovelily.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
